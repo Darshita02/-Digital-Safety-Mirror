308fae0d6b39af33342e106eab260fc6e245246f
pillaiabhiraj07-coder <pillaiabhiraj07@gmail.com>
2026-03-28 12:26:58 +0530

    updated camera.py

--- camera.py
+++ camera.py
@@ -1,14 +1,15 @@
 import cv2
 
 def start_camera():
-    cap = cv2.VideoCapture(0)
-    return cap
-
+    return cv2.VideoCapture(0)
 
 def get_frame(cap):
     ret, frame = cap.read()
-
     if not ret:
         return None
 
+    frame = cv2.flip(frame, 1)
     return frame
+
+def release_camera(cap):
+    cap.release()
