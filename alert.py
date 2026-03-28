12f142b4023211063c1eb6472bacae0b57180e96
marixcv <mariashaikh116@gmail.com>
2026-03-28 19:58:43 +0530

    updated alert.py

--- alert.py
+++ alert.py
@@ -1,13 +1,34 @@
 import streamlit as st
-
+import os
 
 def show_alert(message):
     st.error(message)
 
-
 def show_warning(message):
     st.warning(message)
 
-
 def show_success(message):
     st.success(message)
+
+def play_sound():
+    os.system("echo \a")
+
+
+# OPTIONAL SMS ALERT
+def send_sms_alert():
+    try:
+        from twilio.rest import Client
+
+        account_sid = "your_sid"
+        auth_token = "your_auth_token"
+
+        client = Client(account_sid, auth_token)
+
+        client.messages.create(
+            body="⚠ Unsafe condition detected!",
+            from_="+1234567890",
+            to="+91XXXXXXXXXX"
+        )
+
+    except:
+        print("SMS not configured")
--- safety_detection.py
+++ safety_detection.py
@@ -1,40 +1,41 @@
-def detect_fall(landmarks):
+import math
 
-    if len(landmarks) < 25:
+def calculate_angle(a, b, c):
+    angle = math.degrees(
+        math.atan2(c[1]-b[1], c[0]-b[0]) -
+        math.atan2(a[1]-b[1], a[0]-b[0])
+    )
+    if angle < 0:
+        angle += 360
+    return angle
+
+
+def detect_fall(landmarks):
+    if len(landmarks) < 26:
         return False
 
     shoulder = landmarks[11]
     hip = landmarks[23]
+    knee = landmarks[25]
 
-    if abs(shoulder[1] - hip[1]) < 30:
-        return True
+    angle = calculate_angle(shoulder, hip, knee)
 
-    return False
-
-
-def detect_bad_posture(landmarks):
-
-    if len(landmarks) < 12:
-        return False
-
-    shoulder = landmarks[11]
-    ear = landmarks[7]
-
-    if abs(shoulder[0] - ear[0]) > 40:
-        return True
-
-    return False
+    return angle < 120
 
 
 def detect_imbalance(landmarks):
+    left_shoulder = landmarks[11]
+    right_shoulder = landmarks[12]
 
-    if len(landmarks) < 25:
-        return False
+    tilt = abs(left_shoulder[1] - right_shoulder[1])
 
-    left_hip = landmarks[23]
-    right_hip = landmarks[24]
+    return tilt > 0.05
 
-    if abs(left_hip[1] - right_hip[1]) > 35:
-        return True
 
-    return False
+def detect_unstable_body(landmarks):
+    head = landmarks[0]
+    hip = landmarks[23]
+
+    height = abs(head[1] - hip[1])
+
+    return height < 0.2
